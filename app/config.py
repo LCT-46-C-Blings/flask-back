7207bdfd71e9a34b954d398d0e987fe26f58f97b
zverinus <presentdragons@yandex.ru>
2025-10-02 20:04:43 +0300

    fix

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    FLASK_RUN_HOST = os.getenv("FLASK_RUN_HOST", "0.0.0.0")
+    FLASK_RUN_HOST = os.getenv("FLASK_RUN_HOST", "localhost")
     FLASK_RUN_PORT = os.getenv("FLASK_RUN_PORT", "5050")
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.getenv('DATABASE_URL', 'db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
